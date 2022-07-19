7664d657f0aac7dcfb9813b096169494f8bfe4d5
Victor Gomez <39098525+Victin09@users.noreply.github.com>
2022-07-19 16:15:30 +0000

    fix gd library

--- vam/captcha/simple-php-captcha.php
+++ vam/captcha/simple-php-captcha.php
@@ -9,9 +9,9 @@
 function simple_php_captcha($config = array()) {
 
     // Check for GD library
-    if( !function_exists('gd_info') ) {
-        throw new Exception('Required GD library is missing');
-    }
+    // if( !function_exists('gd_info') ) {
+    //     throw new Exception('Required GD library is missing');
+    // }
 
     $bg_path = dirname(__FILE__) . '/backgrounds/';
     $font_path = dirname(__FILE__) . '/fonts/';
